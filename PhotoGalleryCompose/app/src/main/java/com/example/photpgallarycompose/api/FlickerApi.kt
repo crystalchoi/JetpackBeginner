02360ecd2fd09bb3c5a82c92ef4519f79ec2b49e
crystal <crystal@test.com>
2023-03-30 20:44:01 +0900

    shop

---
+++ PhotoGalleryCompose/app/src/main/java/com/example/photpgallarycompose/api/FlickerApi.kt
@@ -0,0 +1,22 @@
+package com.example.photpgallarycompose.api
+
+import retrofit2.http.GET
+
+
+private const val API_KEY = "97be5666ff4dcfe499ed19a7a5e06c5e"
+
+
+// https://api.flickr.com/services/rest/?method=flickr.interestingness.getList&api_key=yourApiKeyHere&format=json&nojsoncallback=1&extras=url_s
+// https://api.flickr.com/services/rest/?method=flickr.interestingness.getList&api_key=97be5666ff4dcfe499ed19a7a5e06c5e&format=json&nojsoncallback=1&extras=url_s
+
+interface FlickrApi {
+    @GET("services/rest/?method=flickr.interestingness.getList"
+        + "&api_key=$API_KEY" + "&format=json"
+        + "&nojsoncallback=1"
+        + "&extras=url_s"
+    )
+    suspend fun fetchPhotos(): String
+
+    @GET("/")
+    suspend fun fetchContents(): String
+}
