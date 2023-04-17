09a0a0713110f96572db228e4cb101e787b137cc
crystal <sujeungchoi@gmail.com>
2023-04-17 18:32:58 +0900

    Neil/NavigationDemo 완료

--- neilsmith/46.NavigationDemo/app/src/main/java/com/example/navigationdemo/NavRoutes.kt
+++ neilsmith/46.NavigationDemo/app/src/main/java/com/example/navigationdemo/NavRoutes.kt
@@ -1,3 +1,7 @@
 package com.example.navigationdemo
 
-sealed class NavRoutes
+sealed class NavRoutes(private val route: String) {
+    object Home : NavRoutes("home")
+    object Welcome : NavRoutes("welcome")
+    object Profile : NavRoutes("profile")
+}
